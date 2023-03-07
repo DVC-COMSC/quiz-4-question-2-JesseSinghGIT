3f3bb0eb42bfc887a5b869010cd2e685876742fd
JesseSinghGIT <123704475+JesseSinghGIT@users.noreply.github.com>
2023-03-07 09:14:57 +0000

    program

--- q2.py
+++ q2.py
@@ -1,6 +1,32 @@
 
 # ******************************
 # Make your Code
+
+
+
+numberflag = 0
+minstringlen = 0
+maxstringlen = 0
+
+
+
+boolval = True
+while boolval == True:
+    str = input('Enter a string value: ')
+    strlen = len(str)
+    if (numberflag == 0):
+        minstringlen = strlen + 1 
+        maxstring = ''
+    if (str == 'stop' or str == 'STOP'):
+        boolval = False 
+    if (len(str) > len(maxstring)):
+        maxstring = str
+        maxstringlen = len(str)
+    if (len(str) < minstringlen):
+        minstring = str
+        minstringlen = len(str)
+    numberflag = 1
+print (maxstring, minstring)
 # ******************************
 
 
